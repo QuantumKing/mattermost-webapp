69e6e31de60f056b06cbd6ea1dcfc7c077078912
Eric Webster <eric.webster.123@gmail.com>
2017-11-12 15:23:46 -0500

    remove web_websocket_client

--- client/web_websocket_client.jsx
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
-// See License.txt for license information.
-
-import WebSocketClient from 'mattermost-redux/client/websocket_client';
-
-var WebClient = new WebSocketClient();
-export default WebClient;
